d0219f36bcf7cb134917bffa164329f452c6b7e9
Paggy <bhangaleparags@gmail.com>
2018-05-24 17:59:25 +0530

    Conguration WebMvcInitializer
    -internalviewResolver
    -resourcesahndler
      methods

--- founders/src/main/java/in/founders/main/configuration/WebmvcInitializer.java
+++ founders/src/main/java/in/founders/main/configuration/WebmvcInitializer.java
@@ -1,5 +1,14 @@
 package in.founders.main.configuration;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
 /*Created By Parag Bhangale
  * Email:paragbhangale@hotmail.com
  * 
@@ -7,6 +16,43 @@ package in.founders.main.configuration;
  * */
 
 
-public class WebmvcInitializer {
+@Configuration
+@EnableWebMvc
+@ComponentScan("in.founders.main")
+public class WebmvcInitializer extends WebMvcConfigurerAdapter
+{
 
+	/*Auther-Parag Bhangale
+	 * 
+	 * Internal View Resolver is used for the resolve the view
+	 * 
+	 * */
+	@Bean
+	public InternalResourceViewResolver getViewResolver()
+	{
+		InternalResourceViewResolver viewresolver=new InternalResourceViewResolver();
+		viewresolver
+					.setPrefix("/WEB-INF/views/");
+		viewresolver
+					.setSuffix(".jsp");
+		return viewresolver;
+		
+	}
+	
+	
+	
+	/*Auther-Parag Bhangale*/
+	/*
+	 * 
+	 *This method is use for the handling and accessing the external resources 
+	 *in the spring like .js,.css,images etc
+	 * 
+	 * */
+	public void resourceHandler(ResourceHandlerRegistry resourceHandlerRegistry)
+	{
+		resourceHandlerRegistry
+								.addResourceHandler("/resources/**")
+								.addResourceLocations("/resources/");
+	}
+	
 }
